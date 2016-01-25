4987d12841fe6f774681e8c7026d81021b0811ae
Max Strübing <max_struebing@gmx.de>
2016-01-25 16:00:20 +0100

    [TASK] Add basic google search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('var');
+import open from 'open';
+import { sprintf } from 'sprintf-js';
+
+const searchEngines = {
+  google: 'https://www.google.com/search?q=%s',
+};
+
+function printUsage(err) {
+  if (err) {
+    console.error('ERROR:', err);
+  }
+  console.log('web-s searchstring');
+}
+
+function parseArguments(args) {
+  open(sprintf(searchEngines.google, args[0]));
+}
+
+
+if (process.argv.length >= 3) {
+  parseArguments(process.argv.slice(2));
+} else {
+  printUsage();
+}
+console.log(process.argv);
