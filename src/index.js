eeb19ef9b2afcc9bbe72b8f164d181cd38321593
Max Strübing <max_struebing@gmx.de>
2016-02-14 12:33:40 +0100

    [TASK] remove argument consts

--- src/index.js
+++ src/index.js
@@ -3,34 +3,27 @@
 import open from 'open';
 import fs from 'fs';
 
-const ALL_ARGS = 0;
-const ALL_EXCEPT_FIRST_ARG = 1;
 const CONFIG_FILE = `${process.env.HOME}/.web-s.conf`;
 
 const sampleConfig = {
   google: {
     url: 'https://www.google.com/search?q=',
-    args: ALL_ARGS,
     shortHand: 'default',
   },
   twitter: {
     url: 'https://twitter.com/search?src=typd&q=%23',
-    args: ALL_EXCEPT_FIRST_ARG,
     shortHand: 't',
   },
   reddit: {
     url: 'https://www.reddit.com/search?q=',
-    args: ALL_EXCEPT_FIRST_ARG,
     shortHand: 'r',
   },
   stackoverflow: {
     url: 'http://stackoverflow.com/search?q=',
-    args: ALL_EXCEPT_FIRST_ARG,
     shortHand: 's',
   },
   leo: {
     url: 'http://dict.leo.org/ende/index_de.html#/search=',
-    args: ALL_EXCEPT_FIRST_ARG,
     shortHand: 'l',
   },
 };
@@ -68,14 +61,14 @@ function printUsage(err) {
  * joins the arguments dependent on the provider and opens the default webbrowser
  * @param  {object} provider
  * @param  {array} args the cli arguments
- * @return {true}
+ * @return {Boolean}
  */
 function openBrowser(provider, args) {
   let url = 0;
 
-  if (provider.args === ALL_ARGS) {
+  if (provider.shortHand === 'default') {
     url = provider.url + args.join(' ');
-  } else if (provider.args === ALL_EXCEPT_FIRST_ARG) {
+  } else {
     url = provider.url + args.slice(1).join(' ');
   }
 
@@ -102,12 +95,16 @@ function parseArguments(args) {
 }
 
 
+/**
+ * checks with provider should be used
+ * @param  {Array} args
+ * @return {void}
+ */
 function detetermineProvider(args) {
   let opened = false;
   let defaultProvider = false;
 
   Object.keys(searchEngines).forEach((provider) => {
-    console.log(provider);
     if (`-${searchEngines[provider].shortHand}` === args[0] || `--${provider}` === args[0]) {
       opened = openBrowser(searchEngines[provider], args);
     }
@@ -117,7 +114,6 @@ function detetermineProvider(args) {
   });
 
   if (!opened) {
-    console.log('defaultProvider', defaultProvider);
     openBrowser(searchEngines[defaultProvider], args)
   }
 }
